623ab6f8082b968d472297cbf3362f0f1b3dee77
Nick Batsaras <batsarasnikos@gmail.com>
2019-02-02 13:31:04 +0200

    Refactor fiodriver.sh

--- fiodriver.sh
+++ fiodriver.sh
@@ -11,23 +11,15 @@ OUTPUT="FIO-${OUTPUT}"
 mkdir "$OUTPUT"
 
 for DEVICE in ${DEVICES}; do
-	# RANDOM WRITES
 	for IOD in ${IODEPTH[@]}; do
+		# RANDOM WRITES
 		./runfio.sh -d "$DEVICE" -n "$THREADS" -i "$IOD" -f "${FIO_SCRIPTS}/rand-write.fio" -o "${OUTPUT}/rand_w_${DEVICE}_${IOD}iodepth_${THREADS}threads"
-	done
+		# RANDOM READS
+		./runfio.sh -d "$DEVICE" -n "$THREADS" -i "$IOD" -f "${FIO_SCRIPTS}/rand-read.fio"  -o "${OUTPUT}/rand_r_${DEVICE}_${IOD}iodepth_${THREADS}threads"
 
-	# SEQUENTIAL WRITES
-	for IOD in ${IODEPTH[@]}; do
+		# SEQUENTIAL WRITES
 		./runfio.sh -d "$DEVICE" -n 1 -i "$IOD" -f "${FIO_SCRIPTS}/write.fio" -o "${OUTPUT}/seq_w_${DEVICE}_${IOD}iodepth"
-	done
-
-	# RANDOM READS
-	for IOD in ${IODEPTH[@]}; do
-		./runfio.sh -d "$DEVICE" -n "$THREADS" -i "$IOD" -f "${FIO_SCRIPTS}/rand-read.fio" -o "${OUTPUT}/rand_r_${DEVICE}_${IOD}iodepth_${THREADS}threads"
-	done
-
-	# SEQUENTIAL READS
-	for IOD in ${IODEPTH[@]}; do
-		./runfio.sh -d "$DEVICE" -n 1 -i "$IOD" -f "${FIO_SCRIPTS}/read.fio" -o "${OUTPUT}/seq_r_${DEVICE}_${IOD}iodepth"
+		# SEQUENTIAL READS
+		./runfio.sh -d "$DEVICE" -n 1 -i "$IOD" -f "${FIO_SCRIPTS}/read.fio"  -o "${OUTPUT}/seq_r_${DEVICE}_${IOD}iodepth"
 	done
 done
