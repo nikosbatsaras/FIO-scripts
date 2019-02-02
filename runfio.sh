58e356aa3459fc6d49acfd12dc8ef19d513fb18f
Nick Batsaras <batsarasnikos@gmail.com>
2019-02-02 02:12:19 +0200

    Echo information while running

--- runfio.sh
+++ runfio.sh
@@ -78,12 +78,24 @@ if [ -z $directory ]; then
         usage
 fi
 
+echo
+echo "==========================="
+echo "Staring run with:"
+echo "  Device:  $blockdevice"
+echo "  IODEPTH: $iodepth"
+echo "  NUMJOBS: $njobs"
+echo
+echo -n "  Block Size: "
 for bs in ${block_sizes[@]}; do
 	if [ $(cat "/sys/block/${blockdevice}/queue/rotational") -eq 0 ]; then
 		sudo blkdiscard /dev/"$blockdevice"
 	fi
+	echo -n "${bs}KB "
 	{ sudo SIZE='80%' BLOCK_SIZE="${bs}k" DEVICE="$blockdevice" IODEPTH="$iodepth" NJOBS="$njobs" fio "$file" ; } 2>&1 >> "${directory}/${bs}.txt"
 done
+echo
+echo "==========================="
+echo
 
 str="$(fio --version)"
 version="${str:4:1}"
